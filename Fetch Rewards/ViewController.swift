037006e9db0883709773bdf5cfa6866fe1e096e9
Edvin Lellhame <edvinlel@gmail.com>
2020-12-17 13:22:40 -0800

    Initial Commit

---
+++ Fetch Rewards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fetch Rewards
+//
+//  Created by Edvin Lellhame on 12/17/20.
+//  Copyright © 2020 Edvin Lellhame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
